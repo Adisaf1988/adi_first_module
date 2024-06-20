fa6efdeb9b30a55fe353be0b466fbd8034cd2020
Adi Safar <adisafar@Adis-MacBook-Pro.local>
2024-06-20 23:11:12 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("This is my first module");
+function adiModule() {
+    const adi = {name:"Adi", age:36, state:"israel"}
+    if (adi.age>30) {
+        console.log("Hi Adi, your age is over 30");
+    }else{
+        console.log("Hi Adi, your age is less then 30");
+    }
+}
+
+adiModule.export={adiModule}
